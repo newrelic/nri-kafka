b60ecb7d402b89be7dce91488444520592fb466c
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-02 10:49:23 -0400

    Capitalized log messages

--- args/kafka_args.go
+++ args/kafka_args.go
@@ -49,7 +49,7 @@ func ParseArgs(a ArgumentList) (*KafkaArguments, error) {
 	var zookeeperHosts []*ZookeeperHost
 	err := json.Unmarshal([]byte(a.ZookeeperHosts), &zookeeperHosts)
 	if err != nil {
-		logger.Errorf("failed to parse zookeepers from json")
+		logger.Errorf("Failed to parse zookeepers from json")
 		return nil, err
 	}
 
@@ -63,21 +63,21 @@ func ParseArgs(a ArgumentList) (*KafkaArguments, error) {
 	// Parse consumers
 	consumers, err := unmarshalJMXHosts([]byte(a.Consumers), &a)
 	if err != nil {
-		logger.Errorf("failed to parse consumers from json")
+		logger.Errorf("Failed to parse consumers from json")
 		return nil, err
 	}
 
 	// Parse producers
 	producers, err := unmarshalJMXHosts([]byte(a.Producers), &a)
 	if err != nil {
-		logger.Errorf("failed to parse producers from json")
+		logger.Errorf("Failed to parse producers from json")
 		return nil, err
 	}
 
 	// Parse topics
 	var topics []string
 	if err = json.Unmarshal([]byte(a.TopicList), &topics); err != nil {
-		logger.Errorf("failed to parse topics from json")
+		logger.Errorf("Failed to parse topics from json")
 		return nil, err
 	}
 
