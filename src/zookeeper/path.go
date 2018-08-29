af5dd12dbdcce60660d0fa6b32f8ef997ba6c6f6
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-29 10:36:55 -0400

    Clarify comment

--- src/zookeeper/path.go
+++ src/zookeeper/path.go
@@ -6,7 +6,7 @@ import (
 	"github.com/newrelic/nri-kafka/src/args"
 )
 
-// Path takes a zookeeper path and prepends it with the kafka root path
+// Path takes a zookeeper path and prepends it with argument args.GlobalArgs
 func Path(path string) string {
 	return fmt.Sprintf("%s%s", args.GlobalArgs.ZookeeperPath, path)
 }
