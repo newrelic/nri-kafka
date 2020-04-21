db7f1e629f93ee8db5d467d8dd1ec72dbd307f05
Camden Cheek <camden@ccheek.com>
2020-04-20 21:59:58 -0400

    Fix linting

--- src/connection/sasl_scram.go
+++ src/connection/sasl_scram.go
@@ -8,15 +8,20 @@ import (
 	"github.com/xdg/scram"
 )
 
+// SHA256 is a scram.HashGeneratorFcn
 var SHA256 scram.HashGeneratorFcn = func() hash.Hash { return sha256.New() }
+
+// SHA512 is a scram.HashGeneratorFcn
 var SHA512 scram.HashGeneratorFcn = func() hash.Hash { return sha512.New() }
 
+// XDGSCRAMClient implements sarama.SCRAMClient
 type XDGSCRAMClient struct {
 	*scram.Client
 	*scram.ClientConversation
 	scram.HashGeneratorFcn
 }
 
+// Begin is an implementation of sarams.SCRAMClient.Begin()
 func (x *XDGSCRAMClient) Begin(userName, password, authzID string) (err error) {
 	x.Client, err = x.HashGeneratorFcn.NewClient(userName, password, authzID)
 	if err != nil {
@@ -26,11 +31,13 @@ func (x *XDGSCRAMClient) Begin(userName, password, authzID string) (err error) {
 	return nil
 }
 
+// Step is an implementation of sarams.SCRAMClient.Step()
 func (x *XDGSCRAMClient) Step(challenge string) (response string, err error) {
 	response, err = x.ClientConversation.Step(challenge)
 	return
 }
 
+// Done is an implementation of sarams.SCRAMClient.Done()
 func (x *XDGSCRAMClient) Done() bool {
 	return x.ClientConversation.Done()
 }
