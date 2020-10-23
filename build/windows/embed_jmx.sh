1b207e9655a7a14d31f7c5bf5faf096ea549dc86
Joan Porta <jportasa@gmail.com>
2020-10-23 18:27:27 +0200

    Fix embed jmx bash script

--- build/windows/embed_jmx.sh
+++ build/windows/embed_jmx.sh
@@ -5,6 +5,7 @@ set -e
 # Gets last version of nrjmx.jar and nrjmx.bat
 #
 #
-curl https://raw.githubusercontent.com/newrelic/nrjmx/master/bin/nrjmx.bat --output nrjmx.bat
 latest_tag=$(curl --silent "https://api.github.com/repos/${JMX_REPO}/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
-curl -SL http://download.newrelic.com/infrastructure_agent/binaries/linux/noarch/nrjmx_linux_${latest_tag}_noarch.tar.gz | tar xz; cp usr/bin/nrjmx.jar nrjmx.jar
+curl -SL https://github.com/newrelic/nrjmx/releases/download/v1.6.0/nrjmx-${latest_tag}.tar.gz | tar xz
+cp nrjmx-${latest_tag}/lib/nrjmx-${latest_tag}.jar nrjmx.jar
+cp nrjmx-${latest_tag}/bin/nrjmx.bat nrjmx.bat
