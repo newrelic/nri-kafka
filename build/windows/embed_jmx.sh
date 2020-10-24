0ac6bfbf8abf6ee73a320eb172387674cdb6c3f7
Joan Porta <jportasa@gmail.com>
2020-10-24 03:25:52 +0200

    Fix NRJMX_VERSION

--- build/windows/embed_jmx.sh
+++ build/windows/embed_jmx.sh
@@ -5,6 +5,7 @@ set -e
 # Gets last version of nrjmx.jar and nrjmx.bat
 #
 #
+echo "Downlading last version of nrjmx.jar and nrjmx.bat"
 latest_tag=$(curl --silent "https://api.github.com/repos/${JMX_REPO}/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
 curl -SL https://github.com/newrelic/nrjmx/releases/download/v1.6.0/nrjmx-${latest_tag}.tar.gz | tar xz
 cp nrjmx-${latest_tag}/lib/nrjmx-${latest_tag}.jar nrjmx.jar
