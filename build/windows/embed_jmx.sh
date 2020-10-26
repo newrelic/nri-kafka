258cddd6609ff94f1b8fd9edbbdd08ecb08080d5
Grigorii Merkushev <brushknight@gmail.com>
2020-10-26 12:17:38 +0100

    ci: minor updates and cleanup

--- build/windows/embed_jmx.sh
+++ build/windows/embed_jmx.sh
@@ -6,7 +6,7 @@ set -e
 #
 #
 echo "Downlading last version of nrjmx.jar and nrjmx.bat"
-latest_tag=$(curl --silent "https://api.github.com/repos/${JMX_REPO}/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
-curl -SL https://github.com/newrelic/nrjmx/releases/download/v1.6.0/nrjmx-${latest_tag}.tar.gz | tar xz
+latest_tag=$(curl --silent "https://api.github.com/repos/newrelic/nrjmx/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
+curl -SL https://github.com/newrelic/nrjmx/releases/download/v${latest_tag}/nrjmx-${latest_tag}.tar.gz | tar xz
 cp nrjmx-${latest_tag}/lib/nrjmx-${latest_tag}.jar nrjmx.jar
 cp nrjmx-${latest_tag}/bin/nrjmx.bat nrjmx.bat
