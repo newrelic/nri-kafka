bc630aed14935245c4a1c6c98556c21c8ace9029
Roberto Santalla <rsantalla@newrelic.com>
2021-01-13 12:39:37 +0100

    ci: fix nrjmx download script

--- build/windows/embed_jmx.sh
+++ build/windows/embed_jmx.sh
@@ -5,8 +5,13 @@ set -e
 # Gets last version of nrjmx.jar and nrjmx.bat
 #
 #
-echo "Downlading last version of nrjmx.jar and nrjmx.bat"
-latest_tag=$(curl --silent "https://api.github.com/repos/newrelic/nrjmx/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
-curl -SL https://github.com/newrelic/nrjmx/releases/download/v${latest_tag}/nrjmx-${latest_tag}.tar.gz | tar xz
-cp nrjmx-${latest_tag}/lib/nrjmx-${latest_tag}.jar nrjmx.jar
-cp nrjmx-${latest_tag}/bin/nrjmx.bat nrjmx.bat
+echo "Fetching latest nrjmx version..."
+latest_tag=$(curl --silent "https://api.github.com/repos/newrelic/nrjmx/releases/latest" | grep '"tag_name":' | grep -oE 'v([0-9]\.?)+' | sed 's/v//')
+
+echo "Downloading nrjmx_windows_${latest_tag}_amd64.zip..."
+wget --no-verbose "https://github.com/newrelic/nrjmx/releases/download/v${latest_tag}/nrjmx_windows_${latest_tag}_amd64.zip" -O nrjmx_windows.zip
+# TODO(roobre): This is broken an needs fixing
+unzip -p "nrjmx_windows.zip" nrjmx-${latest_tag}/bin/nrjmx.bat > nrjmx.bat
+
+echo "Downloading nrjmx-${latest_tag}-noarch.jar..."
+wget --no-verbose "https://github.com/newrelic/nrjmx/releases/download/v${latest_tag}/nrjmx-${latest_tag}-noarch.jar" -O nrjmx.jar
