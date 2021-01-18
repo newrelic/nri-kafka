54a85a758176247cb0d26970de399e810dda9a16
Roberto Santalla <rsantalla@newrelic.com>
2021-01-18 16:34:15 +0100

    ci: always embed nrjmx 1.5.0

--- build/windows/embed_jmx.sh
+++ build/windows/embed_jmx.sh
@@ -6,7 +6,12 @@ set -e
 #
 #
 echo "Downlading last version of nrjmx.jar and nrjmx.bat"
-latest_tag=$(curl --silent "https://api.github.com/repos/newrelic/nrjmx/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
-curl -SL https://github.com/newrelic/nrjmx/releases/download/v${latest_tag}/nrjmx-${latest_tag}.tar.gz | tar xz
-cp nrjmx-${latest_tag}/lib/nrjmx-${latest_tag}.jar nrjmx.jar
-cp nrjmx-${latest_tag}/bin/nrjmx.bat nrjmx.bat
+if [[ -z $NRJMX_VERSION ]]; then
+    NRJMX_VERSION=$(curl --silent "https://api.github.com/repos/newrelic/nrjmx/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/' | cut -d v -f2)
+    echo "Using latest nrjmx version $NRJMX_VERSION."
+    echo "This script will not work for NRJMX 1.5.1 and onwards."
+fi
+
+curl -SL https://github.com/newrelic/nrjmx/releases/download/v${NRJMX_VERSION}/nrjmx-${NRJMX_VERSION}.tar.gz | tar xz
+cp nrjmx-${NRJMX_VERSION}/lib/nrjmx-${NRJMX_VERSION}.jar nrjmx.jar
+cp nrjmx-${NRJMX_VERSION}/bin/nrjmx.bat nrjmx.bat
