17f4b922f5b86fc72fdd1de42c00acc6c1305f1f
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-06-16 17:36:40 +0200

    extend JMXConnection to allow QueryMBeanNames

--- src/connection/jmx.go
+++ src/connection/jmx.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+
 	"github.com/newrelic/nri-kafka/src/args"
 	"github.com/newrelic/nrjmx/gojmx"
 	"golang.org/x/sync/semaphore"
@@ -63,6 +64,7 @@ func (p *JMXProviderWithConnectionsLimit) NewConnection(config *gojmx.JMXConfig)
 // JMXConnection interface for JMX connection.
 type JMXConnection interface {
 	QueryMBeanAttributes(mBeanNamePattern string) ([]*gojmx.AttributeResponse, error)
+	QueryMBeanNames(mBeanPattern string) ([]string, error)
 	Close() error
 }
 
@@ -75,6 +77,10 @@ type jmxConnection struct {
 
 func (j *jmxConnection) QueryMBeanAttributes(mBeanNamePattern string) ([]*gojmx.AttributeResponse, error) {
 	return j.Client.QueryMBeanAttributes(mBeanNamePattern)
+}
+
+func (j *jmxConnection) QueryMBeanNames(mBeanPattern string) ([]string, error) {
+	return j.Client.QueryMBeanNames(mBeanPattern)
 }
 
 func (j *jmxConnection) Close() error {
