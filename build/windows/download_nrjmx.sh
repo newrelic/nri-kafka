ee993c57de02f1e7956bbeeb763434e0c47bf0bb
rahul <rannareddy@newrelic.com>
2025-09-01 12:34:25 +0530

    reverted download file

--- build/windows/download_nrjmx.sh
+++ build/windows/download_nrjmx.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# Gets last version of nrjmx MSI installer with staging support
+# Gets last version of nrjmx MSI installer
 
-# Set default download base URL (can be overridden with environment variable)
-DOWNLOAD_BASE_URL=${DOWNLOAD_BASE_URL:-"http://nr-downloads-ohai-staging.s3-website-us-east-1.amazonaws.com"}
-
-echo "Downloading last version of nrjmx MSI installer from $DOWNLOAD_BASE_URL"
+echo "Downlading last version of nrjmx MSI installer"
 if [[ -z $NRJMX_URL ]]; then
-    # Use GitHub API to get latest version (this should work regardless of staging/production)
     NRJMX_VERSION=$(curl --silent "https://api.github.com/repos/newrelic/nrjmx/releases/latest" | grep '"tag_name":' |  grep -oE '[0-9.?]+')
     echo "Using latest nrjmx version $NRJMX_VERSION."
-    
-    # Construct URL using the base URL
-    NRJMX_URL=$DOWNLOAD_BASE_URL/infrastructure_agent/binaries/windows/nrjmx/nrjmx-amd64.$NRJMX_VERSION.msi
+    NRJMX_URL=https://github.com/newrelic/nrjmx/releases/download/v$NRJMX_VERSION/nrjmx-amd64.$NRJMX_VERSION.msi
 fi
 
 curl -LSs --fail "$NRJMX_URL" -o "build/package/windows/bundle/nrjmx-amd64.msi"
